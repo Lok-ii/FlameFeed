e38388b81e645b1ef0c3eaa8fbfbdfc5ff4b6579
Lok-ii <lokeshkataria.work@gmail.com>
2024-02-29 17:13:36 +0530

    updated

--- src/Components/Dashboard/DashBoardMainContent.jsx
+++ src/Components/Dashboard/DashBoardMainContent.jsx
@@ -5,15 +5,16 @@ import Carousel from "nuka-carousel";
 import { BsArrowLeftCircleFill } from "react-icons/bs";
 import { BsArrowRightCircleFill } from "react-icons/bs";
 
-
 const DashBoardMainContent = () => {
-  const prevBtn = document.querySelector(".slider-control-centerleft");
-  prevBtn.innerHTML = `<BsArrowLeftCircleFill />`
+  window.onload = () => {
+    const prevBtn = document.querySelector(".slider-control-centerleft");
+    prevBtn.innerHTML = `<BsArrowLeftCircleFill />`;
+  };
   return (
     <div className="w-[85%] flex gap-8">
       <div className="w-[60%] flex justify-end">
         <div className="w-[65%] h-20 flex items-start gap-2 overflow-hidden">
-          <Carousel 
+          <Carousel
             tabbed={false}
             slidesToScroll={4}
             slidesToShow={8}
